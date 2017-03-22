5a12b74a9d4c01e18d9a6ecff1ab90e3f201cafd
sampsonwang <sampsonwang@tencent.com>
2017-03-22 21:29:52 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end('node server start ok.');
+  res.end('node server start ok.update test');
 }).listen(3000, () => {
   console.log('server listen on port: 3000');
 })
