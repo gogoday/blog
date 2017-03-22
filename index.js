e8fb4ed152d3e192393d241a7e2bb07d0d481c0e
sampsonwang <sampsonwang@tencent.com>
2017-03-22 22:52:42 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end('node server start ok.update test, test 2, test 3, test4');
+  res.end('<h1>小桥流水的空间正在建设中</h1>');
 }).listen(3000, () => {
   console.log('server listen on port: 3000');
 })
